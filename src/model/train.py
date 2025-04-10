256c0d78f1eb0140a917802d2023bbe9d2feb96e
cventophd <93550258+cventophd@users.noreply.github.com>
2025-04-10 19:49:34 -0400

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -8,11 +8,13 @@ import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+import mlflow
 
 
 # define functions
 def main(args):
     # TO DO: enable autologging
+    mlflow.autolog()
 
 
     # read data
